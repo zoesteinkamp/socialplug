bebf4d2c7e0bb05fa3d8279e402e378387102d92
Zoe Steinkamp <test@test.com>
2015-03-22 12:16:02 -0700

    Maps now semi-usable

--- main/static/js/swamp.js
+++ main/static/js/swamp.js
@@ -9,48 +9,12 @@ swampdragon.ready(function () {
         }, function (context, data) {
             console.log("Failure to subscribe")
         });
-        swampdragon.onChannelMessage(function (channels, message) {
 
-            console.log(message.data.id);
 
-            if (message.action === "created"){
-
-            }
-            else if( message.action === "deleted"){
-
-            }
-            else if( message.action === "updated"){
-
-            }
-            else{
-
-            }
-
-            });
-
-            getUser();
-            function getUser() {
+        getUser();
+        function getUser() {
                 swampdragon.getSingle('locationcurrent', {id: swamp}, function (context, data) {
                     swampdragon.getList('locationcurrent', {}, function (context, data2) {
-                        console.log(data2);
-                        console.log(data);
-                        otheruser = [];
-                        contentString = "";
-                        data2.forEach(function (item) {
-                            if (item.user === user) {
-                                console.log("found user")
-                            }
-                            else {
-                                prettyarray = [];
-                                prettyarray.push("<h1>" + item.user + "</h1>", item.latitude, item.longititude);
-                                //contentString = '<div id="content">' + '<div id="siteNotice">' + '</div>' +
-                                //'<h1 id="firstHeading" class="firstHeading">' + item.user + '</h1>' + '</div>';
-                                //
-                                otheruser.push(prettyarray)
-                            }
-                        });
-                        console.log(contentString);
-                        console.log(otheruser);
 
                         var myLatlng = new google.maps.LatLng(data.latitude, data.longititude);
                         var mapOptions = {
@@ -59,6 +23,31 @@ swampdragon.ready(function () {
                         };
                         var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+
+                        console.log(data2);
+                        console.log(data);
+                        otheruser = [];
+                        contentString = "";
+
+
+                        data2.forEach(function (item) {
+                            if (item.user === user) {
+                                console.log("found user")
+                            }
+                            else {
+                                prettyarray = [];
+                                prettyarray.push("<h1>" + item.username + "</h1>", item.latitude, item.longititude, item.id);
+                                otheruser.push(prettyarray)
+                            }
+                        });
+                        console.log(contentString);
+                        console.log(otheruser);
+
+
+
+                        function createMarker() {
+                            console.log("Im alivE!")
+                        }
 
                         var infowindow = new google.maps.InfoWindow({
                             maxWidth: 160
@@ -75,29 +64,85 @@ swampdragon.ready(function () {
 
                                 markers.push(marker);
 
-                                google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                                    return function() {
-                                      infowindow.setContent(locations[i][0]);
-                                      infowindow.open(map, marker);
+                                google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                                    return function () {
+                                        infowindow.setContent(locations[i][0]);
+                                        infowindow.open(map, marker);
                                     }
                                 })(marker, i));
                             }
                         }
+                        swampdragon.onChannelMessage(function (channels, message) {
+                        var dragonid = message.data.id;
+                        var lat = message.data.latitude;
+                        var username = message.data.username;
+                        var long = message.data.longititude;
+                        var dragonuser = message.data.user;
+                        if (message.action === "created") {
+                            var uglyarray = []
+                            uglyarray.push("<h1>" + username + "</h1>", lat, long , dragonid);
+                            otheruser.push(uglyarray);
+                            console.log(uglyarray);
+                            setMarkers(map, otheruser);
 
-                        //var marker = new google.maps.Marker({
-                        //    position: myLatlng,
-                        //    map: map,
-                        //    title: 'Hello World!'
-                        //});
+                            //var array = []
+                            //for (var i = 0; i < otheruser.length; i++) {
+                            //     array.push(otheruser[i][3])
+                            //}
+                            //console.log(array)
+
+                            //if (dragonid === array ){
+                            //    console.log("You found me!")
+                            //}
+                        //}
+                        //else if (message.action === "deleted") {
+                        //    var deletearray = [];
+                        //    var originarray = [];
+                        //    originarray.push(["<h1>" + username + "</h1>", lat, long , dragonid]);
+                        //    console.log(originarray);
+                        //
+                        //    for (var i = 0; i < otheruser.length; i++) {
+                        //        for (var j = 0; j < originarray.length; j++){
+                        //            if (otheruser[i] === originarray[j]){
+                        //                var help = otheruser.remove(otheruser[i]);
+                        //                Array.prototype.remove = function(value) {
+                        //                       this.splice(this.indexOf(value), 1);
+                        //                       return true;
+                        //                };
+                        //                console.log(help)
+                        //                console.log(otheruser)
+                        //            }
+                        //            else{
+                        //                console.log("Fuck")
+                        //            }
+                        //        }
+                        //
+                        //    }
+
+
+                            setMarkers(map, otheruser);
+
+                        }
+                        else if (message.action === "updated") {
+
+
+                        }
+                        else {
+                            console.log("Send help, the ship is sinking! SOS!")
+                        }
+
+        });
+
                     }, function (context, data) {
                         console.log("No user with that id? Oh no!")
                     });
                 }, function (context, data) {
                     console.log("no list to return, death to the users!")
                 });
-            };
+            }
 
             google.maps.event.addDomListener(window, 'load', getUser);
+
             //Unsubscribe
             //swampdragon.unsubscribe('locationcurrent', 'local-channel', null, function (context, data) {
             //    console.log("We succesfully unsubsrcibed")
@@ -136,7 +181,7 @@ swampdragon.ready(function () {
             //}, function (context, data) {
             //    console.log("No deleting today")
             //} );
-//        });
-//});
+        });
+});
 
 
