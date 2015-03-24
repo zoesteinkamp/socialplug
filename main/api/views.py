ecbb377e7b3ffeb86d2adade85efe6999e6f068c
Zoe Steinkamp <test@test.com>
2015-03-23 17:15:15 -0700

    Big seach annoying, try again later, maps has weird bug, deal with later. Dealing with distance search for post

--- main/api/views.py
+++ main/api/views.py
@@ -1,26 +1,47 @@
+from itertools import chain
+from rest_framework import generics
 from django.contrib.auth.models import User
+from django.db.models import Q
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from main.api.filter import UserFilter, PostFilter
-from main.api.serializers import SocialAccountSerializer, EventSerializer, UserSerializer
+from main.api.serializers import SocialAccountSerializer, EventSerializer, UserSerializer, GlobalSearchSerializer
 from allauth.socialaccount.models import SocialAccount
 from main.models import Event
 
 
+class GlobalSearchList(generics.ListAPIView):
+    serializer_class = GlobalSearchSerializer
+    paginate_by = 50
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        query = self.request.QUERY_PARAMS.get('query', 'hi')
+        events = Event.objects.filter(Q(title__icontains=query) | Q(category__icontains=query))
+        users = User.objects.filter(username__icontains=query)
+        all_results = list(chain(events, users))
+        all_results.sort(key=lambda x: x.created)
+        return all_results
+
+
 class SocialAccountViewSet(viewsets.ModelViewSet):
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
     queryset = SocialAccount.objects.all()
     serializer_class = SocialAccountSerializer
+    paginate_by = 50
 
 class UserViewSet(viewsets.ModelViewSet):
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
     filter_class = UserFilter
+    paginate_by = 50
 
 class EventViewSet(viewsets.ModelViewSet):
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    paginate_by = 50
     # filter_class = PostFilter
 
     def get_queryset(self):
